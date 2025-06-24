232544e9807d7eacd4baf67c70e31bd496f2aec4
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-06-24 10:30:32 +0600

    In the name of God, the Most Gracious, the Most Merciful.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+import pathlib
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README = (CURRENT_DIR / "readme.md").read_text()
+
+env = os.environ.get('source')
+
+
+def get_dependencies():
+    dependency = [
+        "Quart"
+    ]
+
+    if env and env == "dev":
+        return dependency
+
+    return dependency + []
+
+
+setup(
+    name='flask-quart',
+    version='0.0.1',
+    url='https://github.com/hmtmcse-com/flask-quart',
+    license='Apache 2.0',
+    author='Bangla Fighter',
+    author_email='banglafighter.com@gmail.com',
+    description='Python Code Hub',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=get_dependencies(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ]
+)
