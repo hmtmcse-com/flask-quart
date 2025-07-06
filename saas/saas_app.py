f3f51422874837cee1f81cf05051d6c42664c2fd
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-07 01:03:37 +0600

    Working on SQLAlchemy

--- saas/saas_app.py
+++ saas/saas_app.py
@@ -1,14 +1,8 @@
 from quart import Quart
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from saas.models import load
 from saas.orm.saas_orm import saas_orm
 
 saas_app = Quart(__name__)
-
-
-
-engine = create_async_engine("sqlite+aiosqlite:///example1.db", echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 load()
 
